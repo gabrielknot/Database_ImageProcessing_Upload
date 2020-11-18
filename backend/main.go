cdbc7267753bcc2b422e5310138038cdacb19333
gabrielknot <gcftutos@gmail.com>
2020-11-17 22:45:08 -0300

    Fix wrong connection with data base in this file

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -143,31 +141,7 @@ func configureServer() {
 	log.Fatal(http.ListenAndServe(":3003", router))
 }
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "your-password"
-	dbname   = "calhounio_demo"
-)
-
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully connected!")
 
 	configureServer()
 }
