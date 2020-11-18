f3acabb51aa1e361d138265de0c783dd0f3afaa7
gabrielknot <gcftutos@gmail.com>
2020-11-17 22:46:26 -0300

    Making connection with database

---
+++ backend/createTable.go
@@ -0,0 +1,33 @@
+package createTable
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "imagesDatabase"
+)
+
+func createTable() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
